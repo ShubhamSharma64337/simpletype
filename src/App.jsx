d1787daed45d8a56d24fa7f9dfcdaf67343d82f3
Shubham Sharma <shubhamsharma64337@gmail.com>
2024-07-06 12:53:53 +0530

    Removed useless state variables and updated speed calculation code to use words instead of characters typed

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ function App() {
   const [pressed, setPressed] = useState(['NONE']);
   const [characters, setCharacters] = useState(0);
   const [startTime, setStartTime] = useState(null);
-  const [cpm, setCpm] = useState(0);
   const [givenText, setGivenText] = useState('Machine learning, a subset of AI, continues to be a driving force behind many technological innovations. By leveraging vast amounts of data, machine learning algorithms can identify patterns and make predictions with remarkable precision. This capability is harnessed across a myriad of applications, from personalized recommendations on streaming platforms to predictive maintenance in manufacturing. The healthcare sector, in particular, stands to benefit immensely from machine learning. Predictive analytics can aid in early disease detection, personalized treatment plans, and improved patient outcomes. Nevertheless, the integration of machine learning into healthcare also raises concerns about data security, patient privacy, and the need for robust regulatory frameworks.');
   const [typedText, setTypedText] = useState('');
   const [timeElapsed, setTimeElapsed] = useState(0);
@@ -62,13 +61,7 @@ function App() {
     if(pressedCopy.includes(e.key)){
       let ind = pressedCopy.indexOf(e.key);
       pressedCopy.splice(ind,1);
-      
       setCharacters(characters+1);
-      let dtObj = new Date();
-      let curTime = dtObj.getTime();
-      let timeElapsedInSecs = (curTime - startTime)/1000;
-      
-      setCpm(parseInt(characters*60/timeElapsedInSecs));
     }
     setPressed(pressedCopy);
   }
@@ -79,7 +72,6 @@ function App() {
     setTypedText('');
     setPressed(['NONE']);
     setCharacters(0);
-    setCpm(0);
     setTimeout(()=>{
       setTimeElapsed(0);
       setIsLoading(false);
@@ -95,7 +87,7 @@ function App() {
         <Navbar toggleSettingsModal={toggleSettingsModal}></Navbar>
       </div>
       <div className='row-start-3 flex justify-center'>
-       <SpeedoMeter timeElapsed={timeElapsed} cpm={cpm}></SpeedoMeter>
+       <SpeedoMeter typedText={typedText} timeElapsed={timeElapsed}></SpeedoMeter>
       </div>
       <div className='row-start-4 row-span-6 flex justify-center'>
         <TextDisplay givenText={givenText} typedText={typedText} resetEverything={resetEverything}></TextDisplay>
--- src/components/SpeedoMeter.jsx
+++ src/components/SpeedoMeter.jsx
@@ -1,4 +1,4 @@
-export default function SpeedoMeter({timeElapsed,cpm}) {
+export default function SpeedoMeter({timeElapsed,typedText}) {
   return (
     <div className='text-3xl py-5 flex gap-x-4'>
        <div className="time-elapsed border-r-2 px-5 flex justify-center items-center gap-x-4 text-slate-700">
@@ -13,9 +13,7 @@ export default function SpeedoMeter({timeElapsed,cpm}) {
        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-8">
   <path fillRule="evenodd" d="M12.963 2.286a.75.75 0 0 0-1.071-.136 9.742 9.742 0 0 0-3.539 6.176 7.547 7.547 0 0 1-1.705-1.715.75.75 0 0 0-1.152-.082A9 9 0 1 0 15.68 4.534a7.46 7.46 0 0 1-2.717-2.248ZM15.75 14.25a3.75 3.75 0 1 1-7.313-1.172c.628.465 1.35.81 2.133 1a5.99 5.99 0 0 1 1.925-3.546 3.75 3.75 0 0 1 3.255 3.718Z" clipRule="evenodd" />
 </svg>
-
-
-       {cpm/5} wpm
+       {timeElapsed === 0 ? '0' : parseInt(typedText.split(' ').length*60/timeElapsed)} wpm
        </div>
     </div>
   )
