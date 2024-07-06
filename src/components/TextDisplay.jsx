5c004d1c9f3d2f0d33a7fad9f191713127301fe3
Shubham Sharma <shubhamsharma64337@gmail.com>
2024-07-06 14:15:11 +0530

    Blinking cursor added

--- src/components/TextDisplay.jsx
+++ src/components/TextDisplay.jsx
@@ -7,11 +7,12 @@ export default function TextDisplay({givenText,typedText,resetEverything}) {
       <div className='text-justify w-4/5 sm:w-3/5'>
           {givenArray.map((val, ind) => {
               if (val == typedArray[ind] && ind < typedArray.length) { //Characters which are correctly been typed
-                  return <span key={ind} className={`${ind === typedArray.length - 1 ? 'border-r-2' : ''} ${typedArray.length === 0 && ind === 0? 'border-l-2' : ''} border-purple-500 text-2xl text-purple-700`}>{val}</span>
+                //spans cannot be used dur to certain problems with them, so we will use divs with inline display to animate the cursor border properly
+                  return <div key={ind} className={`${ind === typedArray.length - 1 ? 'border-r-2' : ''} ${typedArray.length === 0 && ind === 0? 'border-l-2' : ''} character inline border-purple-500 text-2xl text-purple-700`}>{val}</div>
                 } else if(val !== typedArray[ind] && ind < typedArray.length){ //Characters which have been typed incorrectly
-                  return <span key={ind} className={`${ind === typedArray.length - 1 ? 'border-r-2' : ''} ${typedArray.length === 0 && ind === 0? 'border-l-2' : ''} border-purple-500 text-2xl ${val===' '?'underline decoration-red-500':'text-red-500'}`}>{val}</span>
+                  return <div key={ind} className={`${ind === typedArray.length - 1 ? 'border-r-2' : ''} ${typedArray.length === 0 && ind === 0? 'border-l-2' : ''} character inline border-purple-500 text-2xl ${val===' '?'underline decoration-red-500':'text-red-500'}`}>{val}</div>
               } else { //Characters not typed yet
-                  return <span key={ind} className={`${ind === typedArray.length - 1 ? 'border-r-2' : ''} ${typedArray.length === 0 && ind === 0? 'border-l-2' : ''} border-purple-500 text-2xl text-slate-400`}>{val}</span>
+                  return <div key={ind} className={`${ind === typedArray.length - 1 ? 'border-r-2' : ''} ${typedArray.length === 0 && ind === 0? 'border-l-2' : ''} character inline border-purple-500 text-2xl text-slate-400`}>{val}</div>
               }
           })}
           <div className='text-slate-600 text-right'>
