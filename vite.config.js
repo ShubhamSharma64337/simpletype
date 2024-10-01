c89d9e18f705547f14a187b6610ecc1545c71f60
Shubham Sharma <shubhamsharma64337@gmail.com>
2024-10-01 10:21:35 +0530

    Fixed a bug which occured in vite.config.js while renaming process.env to import.meta.env in the fetch requests

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: import.meta.env.NODE_ENV === 'production'?'/simpletype':undefined //this is used by vite to set prefix on the static urls while creating the build
+  base: process.env.NODE_ENV === 'production'?'/simpletype':undefined //this is used by vite to set prefix on the static urls while creating the build
 })
