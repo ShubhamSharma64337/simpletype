c627476618245b55221d1be0ab5d4c9aa501f804
Shubham Sharma <shubhamsharma64337@gmail.com>
2024-10-01 12:33:29 +0530

    Added note in the loader informing about the high load time on initial visit

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function Loader({isLoading}) {
   return (
-    isLoading && <div className='loader fixed top-0 left-0 overlay flex justify-center items-center bg-white bg-opacity-50 w-screen h-screen'>
+    isLoading && <div className='loader fixed top-0 left-0 overlay flex flex-col gap-y-2 justify-center items-center bg-white bg-opacity-50 w-screen h-screen z-10'>
         <div className='loader-ring rounded-full p-5 border-4 border-transparent border-r-purple-500 border-t-purple-500'></div>
+        <div className="note bg-white p-2 rounded border"><strong>Note - </strong>It can take around 50s on first visit</div>
     </div>
   )
 }
